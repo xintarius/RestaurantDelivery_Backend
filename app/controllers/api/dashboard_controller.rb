20f22ffe99807fe3a5a6a33b2206d8723cf27542
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2025-09-29 08:25:06 +0200

    update render of backend, add one image to tests for client side step

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -3,7 +3,7 @@ module Api
   before_action :authenticate_user!
 
   def dashboard
-    render json: { email: @current_user.email }
+    render json: { email: @current_user.email, image_url: "/kebab.jpeg" }
   end
   end
 end
