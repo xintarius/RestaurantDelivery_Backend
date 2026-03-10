c95e7a02e4908ac3b67eb79fdf81ffe20436914b
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2026-03-10 14:48:36 +0100

    add rest of courier data for orders view

--- app/channels/orders_channel.rb
+++ app/channels/orders_channel.rb
@@ -1,3 +1,4 @@
+# app/channels/orders_channel.rb
 class OrdersChannel < ApplicationCable::Channel
   def subscribed
     stream_for current_user
@@ -5,20 +6,24 @@ class OrdersChannel < ApplicationCable::Channel
     courier = current_user.courier
     return transmit({ type: 'ERROR', message: "Profil kuriera już istnieje"}) unless courier
 
-    active_order = courier.orders.where(order_status: "ACCEPTED").first
+    active_order = courier.orders.where(order_status: ["ACCEPTED", "IN_DELIVERY"]).first
     if active_order
       puts ">>>> SUBSKRYPCJA: Kurier #{courier.id} ma aktywne zamówienie #{active_order.id}"
+      type = active_order.order_status == "IN_DELIVERY" ? "ORDER_IN_DELIVERY" : "ORDER_ACCEPTED"
+
       transmit({
-                 type: "ORDER_ACCEPTED",
+                 type: type,
                  order: format_order_details(active_order, courier)
                })
     else
-      puts ">>>> SUBSKRYPCJA: Kurier #{courier.id} szuka ofert"
-      orders_scope = courier.orders.where(order_status: 'created').limit(1)
-
-      orders_data = orders_scope.map { |o| format_order_details(o, courier) }
-      transmit({ type: "CURRENT_STATE", orders: orders_data })
+      send_available_orders(courier)
     end
+  end
+
+  def send_available_orders(courier)
+    orders_scope = Order.where(order_status: 'created').limit(1)
+    orders_data = orders_scope.map { |o| format_order_details(o, courier) }
+    transmit({ type: "CURRENT_STATE", orders: orders_data })
   end
 
   def accept_order(data)
@@ -53,8 +58,38 @@ class OrdersChannel < ApplicationCable::Channel
     end
   end
 
+  def pickup_order(data)
+    puts ">>>> PRÓBA PRZEKAZANIA W DOSTAWE DO KLIENTA ZAMÓWIENIA O ID: #{data['order_id']}"
 
-      def unsubscribed
+    courier = current_user&.courier
+    order = Order.find_by(id: data['order_id'])
+
+    if order && order.order_status == 'ACCEPTED'
+      order.update!(order_status: 'IN_DELIVERY')
+      details = format_client_details(order, courier)
+      transmit({ type: "ORDER_IN_DELIVERY", order: details })
+
+      puts ">>>> SUKCES: KURIER O id #{courier.id} jedzie z zamówieniem #{order.id}"
+    else
+      puts ">>>> STATUS NIEOCZEKIWANY: #{order&.order_status}"
+      transmit({ type: "ERROR", message: "Zamówienie już nie jest dostępne (Status: #{order&.order_status})" })
+    end
+  end
+
+  def complete_delivery(data)
+    order = Order.find_by(id: data['order_id'])
+    courier = current_user.courier
+
+    if order && order.order_status == 'IN_DELIVERY'
+      order.update!(order_status: 'delivered')
+      transmit({ type: "DELIVERY_COMPLETED" })
+      orders_scope = Order.where(order_status: 'created').limit(1)
+      orders_data = orders_scope.map { |o| format_order_details(o, courier) }
+      transmit({ type: "CURRENT_STATE", orders: orders_data })
+      puts ">>>> ZAKOŃCZONO: Zamówienie #{order.id} zostało dostarczone!"
+    end
+  end
+  def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
@@ -74,6 +109,24 @@ class OrdersChannel < ApplicationCable::Channel
       vendor_apartment: vendor_address&.apartment,
       vendor_postal_code: vendor_address&.postal_code,
       vendor_city: vendor_address&.city,
+      gross_payment: payment&.gross_payment
+    }
+  end
+
+  def format_client_details(order, courier)
+    payment = order.courier_payments.find { |p| p.courier_id == courier.id }
+    user_address = order.user.address
+
+    {
+      order_id: order.id,
+      order_number: order.order_number,
+      order_status: order.order_status,
+      vendor_name: order.user.username,
+      vendor_street: user_address&.street,
+      vendor_building: user_address&.building,
+      vendor_apartment: user_address&.apartment,
+      vendor_postal_code: user_address&.postal_code,
+      vendor_city: user_address&.city,
       gross_payment: payment&.gross_payment
     }
   end
