26bad9e1f57719facc42344f298a80b9a85009bd
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2026-03-15 01:41:59 +0100

    last fix controller

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -4,7 +4,7 @@ class Api::NotificationsController < ApplicationController
   def get_notification
     notifications = Notification.joins(:roles)
                                 .where(roles: { id: current_user.role_id })
-                                .order(created_at: :desc)
+                                .order('notifications.created_at DESC')
     render json: notifications
   end
 end
