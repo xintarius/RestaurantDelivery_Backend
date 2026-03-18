990600ca9b1e08c05b52c048d3ef86929e508bfc
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2026-03-18 11:00:13 +0100

    fix courier payment in orders

--- app/services/history_service.rb
+++ app/services/history_service.rb
@@ -3,17 +3,20 @@ class HistoryService
 
   def self.generate_history_order(order, courier)
     vendor_address = order.vendor.user.address
+    payment = courier.courier_payments.find_by!(order_id: order.id)
+
+    earnings = payment&.gross_payment || 0
 
     ActiveRecord::Base.transaction do
       HistoryOrder.create!(
         order_id: order.id,
-        courier_id: courier,
+        courier_id: courier.id,
         vendor_id: order.vendor.id,
         restaurant_name: order.vendor.name,
         pickup_address: vendor_address,
         delivery_address: order.user.address,
         order_status: order.order_status,
-        total_courier_earnings: order.total_courier_earnings,
+        total_courier_earnings: earnings,
         base_earnings_data: nil
       )
     end
