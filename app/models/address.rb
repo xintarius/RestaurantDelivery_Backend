7b3cdc4862f7228b9ef4a67415657d4202e34e6d
Grzegorz Jasek <grzegorz.jasek@comp-ci.pl>
2026-03-19 09:51:42 +0100

    create full address in history service for clients and vendors

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,10 @@ class Address < ApplicationRecord
   has_many :users
   belongs_to :location
   has_many :vendors
+
+  def full_address
+    [city, postal_code, postal_city, street, building, apartment]
+      .select(&:present?)
+      .join(', ')
+  end
 end
--- app/services/history_service.rb
+++ app/services/history_service.rb
@@ -2,19 +2,19 @@
 class HistoryService
 
   def self.generate_history_order(order, courier)
-    vendor_address = order.vendor.user.address
-    payment = courier.courier_payments.find_by!(order_id: order.id)
-
+    vendor_address = order.vendor.user.address.full_address
+    client_address = order.user.address.full_address
+    payment = order.courier_payments.find { |p| p.courier_id == courier.id }
     earnings = payment&.gross_payment || 0
 
     ActiveRecord::Base.transaction do
       HistoryOrder.create!(
         order_id: order.id,
-        courier_id: courier.id,
+        courier_id: courier,
         vendor_id: order.vendor.id,
         restaurant_name: order.vendor.name,
         pickup_address: vendor_address,
-        delivery_address: order.user.address,
+        delivery_address: client_address,
         order_status: order.order_status,
         total_courier_earnings: earnings,
         base_earnings_data: nil
